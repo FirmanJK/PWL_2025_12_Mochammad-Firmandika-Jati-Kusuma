efd87e8471510dddf7a6fcabf47c663717734969
FirmanJK <jatikusuma761@gmail.com>
2025-03-11 18:39:19 +0700

    Praktikum 2.6 - Create, Read, Update, Delete (CRUD)

--- Minggu 4/Jobsheet 4/PWL_POS/resources/views/user.blade.php
+++ Minggu 4/Jobsheet 4/PWL_POS/resources/views/user.blade.php
@@ -22,13 +22,11 @@
 </body>
 </html> -->
 
-<!DOCTYPE html>
-<html lang="id">
-<!-- <head> -->
+<!-- <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data User</title>
-    <!-- <style>
+    <style>
         --table {
             --border-collapse: collapse;
             width: 100px;
@@ -40,21 +38,33 @@
         }
     </style>
 </head>-->
+<!DOCTYPE html>
+<html lang="id">
 <body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <td>ID</td>
-            <td>Username</td>
-            <td>Nama</td>
-            <td>ID Level Pengguna</td>
-        </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>
-    </table>
-</body>
+    <body>
+        <h1>Data User</h1>
+        <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+                <th>Aksi</th>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{ $d->user_id }}</td>
+                    <td>{{ $d->username }}</td>
+                    <td>{{ $d->nama }}</td>
+                    <td>{{ $d->level_id }}</td>
+                    <td>
+                        <a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> |
+                        <a href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
 </html>
+
