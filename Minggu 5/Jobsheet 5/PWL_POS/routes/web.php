f8606176da9102d46ab1ff658cae4ef8dad60f17
FirmanJK <jatikusuma761@gmail.com>
2025-03-22 14:58:46 +0700

    Praktikum 1 - Layouting  AdminLTE:

--- Minggu 5/Jobsheet 5/PWL_POS/routes/web.php
+++ Minggu 5/Jobsheet 5/PWL_POS/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
@@ -17,17 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori/create', [KategoriController::class, 'create']);
-Route::post('/kategori', [KategoriController::class, 'store']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit']) -> name('kategori.edit');
-Route::put('/kategori/{id}', [KategoriController::class, 'update']) -> name('kategori.update');
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']) -> name('kategori.destroy');
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
