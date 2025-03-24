e07c2afa7ee1d20284cd26811c317a6b2079eed3
FirmanJK <jatikusuma761@gmail.com>
2025-03-24 11:59:19 +0700

    first commit Jobsheet 6

---
+++ Minggu 6/Jobsheet 6/PWL_POS/app/Models/BarangModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'kategori_id',
+        'barang_kode',
+        'barang_nama',
+        'harga_beli',
+        'harga_jual',
+    ];
+
+    /**
+     * Relasi ke model KategoriModel.
+     */
+    public function kategori()
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+}
