b615078d32bf5fb4f80ef1edf4c2cfccfaf88a37
FirmanJK <jatikusuma761@gmail.com>
2025-04-06 12:45:24 +0700

    Praktikum 3 dan Tugas 3 – Implementasi Multi-Level Authorization

--- Minggu 7/Jobsheet 7/PWL_POS/app/Models/UserModel.php
+++ Minggu 7/Jobsheet 7/PWL_POS/app/Models/UserModel.php
@@ -40,5 +40,13 @@ class UserModel extends Authenticatable
     public function hasRole($role): bool
     {
         return $this->level->level_kode === $role;
-    }   
+    }  
+    
+    /**
+     * Mendapatkan kode role
+     */
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
 }
