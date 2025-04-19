e3fa138a8b4e5b04248cc296861e20b4e8814793
FirmanJK <jatikusuma761@gmail.com>
2025-04-19 12:33:36 +0700

    JS 4 Praktikum 2.7 – Relationships

--- UTS/PWL_POS/app/Models/UserModel.php
+++ UTS/PWL_POS/app/Models/UserModel.php
@@ -13,4 +13,9 @@ class UserModel extends Authenticatable
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
