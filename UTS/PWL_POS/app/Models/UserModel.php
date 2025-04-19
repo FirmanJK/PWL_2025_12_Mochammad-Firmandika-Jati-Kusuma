80c677b2221a7ac7cf68e5002bf04d26ae2a03ac
FirmanJK <jatikusuma761@gmail.com>
2025-04-19 18:07:46 +0700

    JS 7 Praktikum 1 – Implementasi Authentication

--- UTS/PWL_POS/app/Models/UserModel.php
+++ UTS/PWL_POS/app/Models/UserModel.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable
 {
@@ -12,8 +13,15 @@ class UserModel extends Authenticatable
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['username', 'password', 'name', 'level_id', 'created_at', 'updated_at'];
 
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
