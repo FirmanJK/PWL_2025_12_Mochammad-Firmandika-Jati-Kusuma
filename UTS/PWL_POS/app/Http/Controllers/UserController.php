37cfaba12b5e71cfa007dedd01909968712537b6
FirmanJK <jatikusuma761@gmail.com>
2025-04-19 12:58:24 +0700

    JS 6 Praktikum 1. Modal Ajax Tambah Data (Data User)

--- UTS/PWL_POS/app/Http/Controllers/UserController.php
+++ UTS/PWL_POS/app/Http/Controllers/UserController.php
@@ -37,4 +37,44 @@ class UserController extends Controller
         'activeMenu' => $activeMenu
     ]);
 }
+
+public function store_ajax(Request $request)
+{
+    // cek apakah request berupa ajax
+    if ($request->ajax() || $request->wantsJson()) {
+        $rules = [
+            'level_id' => 'required|integer',
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:6'
+        ];
+
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false, // response status, false: error/gagal, true: berhasil
+                'message' => 'Validasi Gagal',
+                'msgField' => $validator->errors() // pesan error validasi
+            ]);
+        }
+
+        // UserModel::create($request->all());
+
+        // Menambahkan perbaikan kode untuk melakukan hash password terlebih dahulu sebelum disimpan ke database
+        $data = $request->all();
+
+        $data['password'] = Hash::make($request->password);
+
+        UserModel::create($data);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data user berhasil disimpan'
+        ]);
+    }
+
+    redirect('/');
+}
 }
