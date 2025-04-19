d2021fb416922c99de1fbf318c11adc4f11cad64
FirmanJK <jatikusuma761@gmail.com>
2025-04-19 12:08:02 +0700

    JS 4 Praktikum 1 - $fillable:

--- UTS/PWL_POS/app/Models/UserModel.php
+++ UTS/PWL_POS/app/Models/UserModel.php
@@ -12,41 +12,5 @@ class UserModel extends Authenticatable
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 
-                            'profile_photo', 'created_at', 'updated_at']; // kolom yang bisa diisi
-    
-    protected $hidden = ['password']; // jangan ditampilkan saat select
-
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
-
-    /**
-     * Relasi ke tabel level
-     */
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-     /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-        return $this->level->level_nama;
-    }
-
-    /**
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode === $role;
-    }  
-    
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole()
-    {
-        return $this->level->level_kode;
-    }
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
