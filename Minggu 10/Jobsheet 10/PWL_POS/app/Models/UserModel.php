90c8f49affd7caee6b80e5f06a0746796ca495b4
FirmanJK <jatikusuma761@gmail.com>
2025-04-27 17:53:00 +0700

    JS 10 Praktikum 2 – Membuat RESTful API Login

--- Minggu 10/Jobsheet 10/PWL_POS/app/Models/UserModel.php
+++ Minggu 10/Jobsheet 10/PWL_POS/app/Models/UserModel.php
@@ -5,8 +5,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 //========================================= Jobsheet 10 Praktikum 1 =========================================
